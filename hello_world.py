31b0831afda1927a7d7114c28cb3e6378cd1f189
Tyler Perron <tperron31313@gmail.com>
2022-09-27 21:01:06 -0400

    first Commit

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+from mailbox import Babyl
+
+
+print('Hello World')
+name = 'Tyler'
+print(name)
+print(f'Hello {name}!')
+print('Hello ' + name + '!')
+favorite_number = 13
+print(f'Hello {favorite_number}!')
+print('Hello ' + favorite_number + '!')
+food1 = 'Cajun Pasta'
+food2 = 'Air Fryer Steak'
+print('I love to eat ' + {food1} + {food2} + '!')
+print(f'I love to eat {food1} and {food2}!')
